1c6c86e771672bbbdf2f00ae5ed4f540cdf8c5b0
王华 <neil_wang1987@sina.cn>
2019-12-26 17:29:46 +0800

    创建我的组件

--- Example/Pods/Target Support Files/WHMyTest/WHMyTest-umbrella.h
+++ Example/Pods/Target Support Files/WHMyTest/WHMyTest-umbrella.h
@@ -10,24 +10,7 @@
 #endif
 #endif
 
-#import "NSArray+Extensions.h"
-#import "NSArray+Log.h"
-#import "NSDictionary+Extensions.h"
-#import "NSDictionary+Log.h"
-#import "NSString+Extensions.h"
-#import "NSUserDefaults+Extensions.h"
-#import "UIBarButtonItem+Extensions.h"
-#import "UIButton+Extensions.h"
-#import "UIColor+Extensions.h"
-#import "UIControl+Extensions.h"
-#import "UIDevice+Extensions.h"
-#import "UIImage+Extensions.h"
-#import "UILabel+Extension.h"
-#import "UINavigationController+extensions.h"
 #import "UIScreen+Extensions.h"
-#import "UITextView+Extensions.h"
-#import "UIView+Extensions.h"
-#import "UIWindow+Extensions.h"
 
 FOUNDATION_EXPORT double WHMyTestVersionNumber;
 FOUNDATION_EXPORT const unsigned char WHMyTestVersionString[];
